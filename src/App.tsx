4876fc56d92fdad0ba271ebf93fbc079987db995
Daria Achkasova <daria.achkasova6@gmail.com>
2026-02-20 21:41:39 +0100

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [originalTodos, setOriginalTodos] = useState<Todo[]>([]);
+  const [displayedTodos, setDisplayedTodos] = useState<Todo[]>([]);
+  const [selectedTodoId, setSelectedTodoId] = useState<number | null>(null);
+
+  useEffect(() => {
+    getTodos().then(todos => {
+      setOriginalTodos(todos);
+      setDisplayedTodos(todos);
+    });
+  }, []);
+
+  function getTodoById(id: number): Todo | undefined {
+    return originalTodos.find(todo => todo.id === id);
+  }
+
+  const handleCloseModal = () => {
+    setSelectedTodoId(null);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +38,33 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                todos={originalTodos}
+                setDisplayedTodos={setDisplayedTodos}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {originalTodos.length === 0 ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={displayedTodos}
+                  selectedTodoId={selectedTodoId}
+                  onSelectTodoId={setSelectedTodoId}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodoId !== null && (
+        <TodoModal
+          todo={getTodoById(selectedTodoId)!}
+          onClose={handleCloseModal}
+        />
+      )}
     </>
   );
 };
